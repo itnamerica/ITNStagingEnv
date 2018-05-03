7b8bdc51472544c238fb7db2685e9b10cd9d59f4
Samantha Guergenenov <samguergen@gmail.com>
2018-05-03 16:16:22 -0400

    es5

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(allPages, function(req, res){
 });
 
 
-MongoClient.connect('mongodb://itnadmin:itnUser0136!@ds263639.mlab.com:63639/itnamerica', (err, client) => {
+MongoClient.connect('mongodb://itnadmin:itnUser0136!@ds263639.mlab.com:63639/itnamerica', function(err, client) {
   if (err) return console.log(err)
 
   app.post('/sendmail', function(req, res){

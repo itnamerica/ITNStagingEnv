61c11c26319bc66efbf435aad001ed78ecf05c11
Samantha Guergenenov <samguergen@gmail.com>
2018-05-22 14:36:45 -0400

    comment resolve on dashboard

--- app/app/app.js
+++ app/app/app.js
@@ -119,16 +119,20 @@ myApp.config(function($stateProvider, $urlRouterProvider, $locationProvider){
       })
       .state('dashboard', {
         url: '/dashboard',
-        templateUrl: viewsPath + 'dashboard.html',
-        resolve: {
-          memberapps : function($http, FormService, $scope){
-            return FormService.getMemberForms().then(function(data){
-              $scope.memberFormData = data;
-              return data;
-            })
-          }
-        }
+        templateUrl: viewsPath + 'dashboard.html'
       })
+      // .state('dashboard', {
+      //   url: '/dashboard',
+      //   templateUrl: viewsPath + 'dashboard.html',
+      //   resolve: {
+      //     memberapps : function($http, FormService, $scope){
+      //       return FormService.getMemberForms().then(function(data){
+      //         $scope.memberFormData = data;
+      //         return data;
+      //       })
+      //     }
+      //   }
+      // })
       .state('wildcard', {
         url: '/*',
         templateUrl: viewsPath + 'home.html'
@@ -203,6 +207,7 @@ myApp.controller('MainController', ['$scope', '$transitions','$http', '$anchorSc
   };
   $scope.dataPDF = null;
   $scope.formType = '';
+  $scope.memberFormData = [];
   $scope.formSubject = 'New application received';
   $scope.states = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
   $scope.itnSources = ['Family','Friend','Speaker','Doctor','Radio','Television','Flier','Book','Phone','Agency on Aging', 'Social Worker','Internet','Referred by Current Member'];
@@ -354,6 +359,17 @@ myApp.controller('MainController', ['$scope', '$transitions','$http', '$anchorSc
     }, stepTime);
   }
 
+  $scope.getMemberApps = function(){
+    FormService.getMemberForms().then(function(data){
+            $scope.memberFormData = data;
+            return data;
+          })
+  }
+  FormService.getMemberForms().then(function(data){
+  //         $scope.memberFormData = data;
+  //         return data;
+  //       })
+  //     }
 
   $scope.submitForm = function(formType){
     console.log('submitForm, formData is', $scope.formData);

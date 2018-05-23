b9412d0e6e64c6b6cf51afd9a3dbecdb1c36b956
Samantha Guergenenov <samguergen@gmail.com>
2018-05-23 16:35:36 -0400

    add date filters to dashboard

--- app/app/app.js
+++ app/app/app.js
@@ -550,6 +550,16 @@ myApp.filter('inputSelected', function(){
 });
 
 
+myApp.filter('timestamp', function(){
+  return function(formObj){
+    console.log('formobj in filter is ', formObj);
+    var timestamp = formObj._id.toString().substring(0,8);
+    var date = new Date( parseInt( timestamp, 16 ) * 1000 );
+    return date;
+  }
+});
+
+
 myApp.service('FormService', function($http){
   this.getMemberForms = function(){
     return $http.get('/getMemberApps').then(function(data){
